2ca08338fb629ccb9327d67110d7a81966bc34f0
johndacosmo <jpali@bellsouth.net>
2020-07-10 17:11:20 -0400

    Add GuessCard.java

---
+++ src/main/java/com/codedifferently/casino/GuessCard.java
@@ -0,0 +1,34 @@
+package com.codedifferently.casino;
+
+public class GuessCard {
+
+
+    public void gameLogic() {
+
+
+        final int CHANCES = 5;
+        int count = 1;
+
+        while (count < CHANCES) {
+
+            String userGuess = "What is your guess: ";
+            System.out.println();
+
+            int guess;
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+
+
+}
